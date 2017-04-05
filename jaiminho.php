c51b01843055592631cc1b7920fab00fd4d7d7a6
Maurilio Atila <cabelotaina@gmail.com>
2017-04-05 17:42:23 -0300

    trying to solve the issue 80

--- jaiminho.php
+++ jaiminho.php
@@ -793,7 +793,7 @@ class Jaiminho extends SendPress
 				'menu'         => 'tgmpa-install-plugins', // Menu slug.
 				'parent_slug'  => 'plugins.php',           // Parent menu slug.
 				'capability'   => 'manage_options',        // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-				'has_notices'  => true,                    // Show admin notices or not.
+				'has_notices'  => false,                    // Show admin notices or not.
 				'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
 				'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
 				'is_automatic' => false,                   // Automatically activate plugins after installation or not.
